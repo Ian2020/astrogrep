8bdf483b3e9f9d1691ec1bfc814219fa2952fa6f
Ian <Ian2020@users.noreply.github.com>
2017-08-24 12:03:39 +0100

    AstroGrep 4.4.5.0

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$packageName = "AstroGrep"
+$url = 'http://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.5/AstroGrep_Setup_v4.4.5.exe'
+Install-ChocolateyPackage $packageName 'exe' '/S' $url
---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+$packageName = "AstroGrep"
+Uninstall-ChocolateyPackage $packageName 'exe' '/S' "C:\Program Files (x86)\AstroGrep\Uninstall.exe"
